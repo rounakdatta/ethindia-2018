2706e4926373cfea1794a1f593d962bec0dbb393
Souradeep <souradeep-das>
2018-08-11 20:44:58 +0530

    updated

--- lend.sol
+++ lend.sol
@@ -1,5 +1,31 @@
 pragma solidity ^0.4.10;
 
+contract Factory {
+
+    address[] public allContracts;
+    uint256 public contractCount;
+    address public factoryOwner;
+
+    function Factory() public {
+        factoryOwner = msg.sender;
+        contractCount = 0;
+    }
+
+    function createContract( uint256 _principal, uint256 _collAmount, uint256 _duration, uint256 _rate, uint256 _loanExpWeeks) public {
+        address newContract = new ethindia(_principal,_collAmount,_duration,_rate,_loanExpWeeks);
+        allContracts.push(newContract);
+        contractCount++;
+    }
+
+    function getAllContracts() public view returns (address[]) {
+        return allContracts;
+    }
+
+    function getByID(uint256 queryID) public view returns (address) {
+        return allContracts[queryID];
+    }
+}
+
 contract ethindia {
     address[] lenders;
     address borrower;
@@ -12,7 +38,7 @@ contract ethindia {
 
     uint256 interest = 0;
 
-    uint256 loanFill = 0;
+    uint256 public loanFill = 0;
     bool loanFilledStatus = false;
 
     uint256 loanRepay = 0;
@@ -21,8 +47,9 @@ contract ethindia {
     uint256 lenderCount = 0;
 
     mapping (int => address) lender;
+    mapping (address => uint) lendertoamount;
 
-    function createLoan( uint256 _principal, uint256 _collAmount, uint256 _duration, uint256 _rate, uint256 _loanExpWeeks) public {
+    function ethindia( uint256 _principal, uint256 _collAmount, uint256 _duration, uint256 _rate, uint256 _loanExpWeeks) public {
         borrower = msg.sender;
         principal = _principal;
         collAmount = _collAmount;
@@ -35,8 +62,12 @@ contract ethindia {
     function payToLoan() public payable {
         require(!loanFilledStatus);
 
-        lenders[lenderCount++] = msg.sender;
+        lenders.push(msg.sender);
+
         loanFill += msg.value;
+
+        lendertoamount[msg.sender]+=msg.value;
+
         if(loanFill == principal) {
             loanFilledStatus = true;
             interest = (principal * duration * rate) / 100;
